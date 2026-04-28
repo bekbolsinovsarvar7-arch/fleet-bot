c48e33329fa846eda343d607ac05a88980536909
bekbolsinovsarvar7-arch <bekbolsinovsarvar7@gmail.com>
2026-04-28 11:22:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ from threading import Thread
 
 app = Flask('')
 
-@app.route('/')def home():return "Bot is alive"
+@app.route('/')
+def home():
+    return "Bot is alive"
 
 def run():app.run(host='0.0.0.0', port=8080)
 
